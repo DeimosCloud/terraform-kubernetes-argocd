2c810db78967e51b8a5f7c2c28c870b8b8a90fad
Mmadu Manasseh <mmadumanasseh@gmail.com>
2022-05-19 12:09:53 +0100

    Fix repository configuration

--- main.tf
+++ main.tf
@@ -1,64 +1,10 @@
 locals {
-  # extra_manifests = join(" ", var.manifests)
-
-  secret_name = "argocd-repository-credentials"
-
-  # Get list of all keys for secrets
-  secret_keys = [
-    for i, repo in var.repositories :
-    ["username-${i}", "password-${i}", "sshkey-${i}", ]
-  ]
-
-  # Get values for each key
-  secret_values = [
-    for repo in var.repositories :
-    [
-      lookup(repo, "username", lookup(repo, "access_token", null) != null ? "argocd" : null),
-      lookup(repo, "password", lookup(repo, "access_token", null)),
-      lookup(repo, "password", lookup(repo, "ssh_key", null))
-    ]
-  ]
-
-  # Delete keys with empty values
-  secrets = {
-    for key, value in zipmap(flatten(local.secret_keys), flatten(local.secret_values)) :
-    key => value if value != null
-  }
-
-  # construct a map of repositories in format
-  # {
-  # type         = repoType
-  # url            = repoURL
-  # usernameSecret = usernameSecret
-  # passwordSecret = passwordSecret
-  # sshSecret      = sshSecret
-  # }
-
-  all_repositories = [
-    for i, repo in var.repositories : {
-      type = lookup(repo, "type", null)
-      url  = repo.url
-      usernameSecret = lookup(repo, "username", lookup(repo, "access_token", null) != null ? "argocd" : null) == null ? null : {
-        key  = "username-${i}"
-        name = local.secret_name
-      }
-      passwordSecret = lookup(repo, "password", lookup(repo, "access_token", null)) == null ? null : {
-        key  = "password-${i}"
-        name = local.secret_name
-      }
-      sshSecret = lookup(repo, "password", lookup(repo, "ssh_key", null)) == null ? null : {
-        key  = "sshkey-${i}"
-        name = local.secret_name
-      }
-    }
-  ]
-
   # Remove map keys from all_repositories with no value. That means they were not specified
-  clean_repositories = [
-    for repo in local.all_repositories : {
+  clean_repositories = {
+    for name, repo in var.repositories : "${name}" => {
       for k, v in repo : k => v if v != null
     }
-  ]
+  }
 
   # If no_auth_config has been specified, set all configs as null
   values = merge({
@@ -69,10 +15,7 @@ locals {
     }
 
     server = {
-      config = merge({
-        # Configmaps require strings, yamlencode the map
-        repositories = yamlencode(local.clean_repositories)
-      }, var.config)
+      config     = var.config
       rbacConfig = var.rbac_config
       # Run insecure mode if specified, to prevent argocd from using it's own certificate
       extraArgs = var.server_insecure ? ["--insecure"] : null
@@ -89,7 +32,8 @@ locals {
       }
     }
     configs = {
-      repositories = local.secrets
+      # Configmaps require strings, yamlencode the map
+      repositories = local.clean_repositories
     }
   })
 }
@@ -105,7 +49,10 @@ resource "helm_release" "argocd" {
   # force_update = true
   # dependency_update = true
 
-  values = [yamlencode(local.values), yamlencode(var.values)]
+  values = concat(
+    [yamlencode(local.values), yamlencode(var.values)],
+    [for x in var.values_files : file(x)]
+  )
 }
 
 data "kubectl_path_documents" "docs" {
@@ -113,7 +60,7 @@ data "kubectl_path_documents" "docs" {
 }
 
 resource "kubectl_manifest" "extra_manifests" {
-  for_each  = toset(data.kubectl_path_documents.docs.documents)
+  for_each  = toset(concat(data.kubectl_path_documents.docs.documents, var.manifests))
   yaml_body = each.value
 
   depends_on = [helm_release.argocd]
